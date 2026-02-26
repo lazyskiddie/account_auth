f7d8123163d6a86367ce066e3755c6d4df4a7fdb
lazyskiddie <sandeepsingh08550@gmail.com>
2026-02-26 12:12:41 +0530

    Update models and cache

--- accounts/models.py
+++ accounts/models.py
@@ -8,6 +8,7 @@ class UserOTP(models.Model):
     otp = models.CharField(max_length=6)
     created_at = models.DateTimeField(auto_now=True)
 
+
     def is_valid(self):
         # OTP is valid for 5 minutes
         return timezone.now() < self.created_at + datetime.timedelta(minutes=5)
